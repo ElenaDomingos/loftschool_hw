bd637f255d43c8eef1cb75cf15c86ccb53129c6f
Elena Domingos <elena-lezhneva@mail.ru>
2019-07-17 21:00:24 -0300

    Add files via upload
    Домашняя работа 1 задание 6

---
+++ hw_1_#6.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<head>
+    <title>Multiply it</title>
+    <style>
+        td {
+            border:1px solid #1d1d1d;
+            padding:10px;
+        }
+    </style>
+</head>
+<body>
+<?php
+
+echo '<table>';
+for($i=1;$i<=10;$i++){
+    echo '<tr>';
+    $y=1;
+    for($x=1;$x<=10;$x++){
+
+        echo '<td>';
+        $res = $i*$y;
+        $y++;
+        if($x>1 && $i>1){
+            if($res%2==0){
+                echo '(' . $res . ')';
+            }
+            else {
+                echo '[' . $res . ']';
+            }
+        }
+        else {
+            echo $res;
+        }
+
+        echo '</td>';
+
+    }
+    echo '</tr>';
+}
+
+echo '</table>';
+
+?>
+
+</body>
+</html>
