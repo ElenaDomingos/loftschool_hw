3469339662660aa5070b694fb82dbab9b7d8a8b2
Elena Domingos <elena-lezhneva@mail.ru>
2019-07-20 15:43:23 -0300

    Create index.php
    functions

---
+++ functions/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta htt p -equi v="Content-Type" conte n t="text/html; charset=utf- 8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        td {
+            border: 1px solid #1d1d1d;
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php
+    require 'src/functions.php';
+
+    task1(['Привет', 'Hello'], false);
+
+    task2('+', 1, 2, 3, 4);
+
+    task3(6,6);
+
+    task4();
+
+    task5();
+
+    task6('text.txt');
+
+    ?>
+
+
+</body>
+
+</html>
