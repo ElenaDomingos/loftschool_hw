9ab938e89f0718fa3da90c3dbe7bacd4ba9c5f3d
Elena Domingos <elena-lezhneva@mail.ru>
2019-07-22 00:28:51 -0300

    Add files via upload

--- functions/src/functions.php
+++ functions/src/functions.php
@@ -3,85 +3,87 @@
 
 //task 1
 
-function task1(array $strings, bool $trueorfalse=false){
-echo '<p>';
-foreach ($strings as $string) {
-    if($trueorfalse=='false'){
-    echo '<p>' . $string . ' </p>';
-    } else {
-        echo $string;
+function task1(array $strings, bool $implode = false)
+{
+    echo '<p>';
+    foreach ($strings as $string) {
+        if (!$implode) {
+            echo '<p>' . $string . ' </p>';
+        } else {
+            echo $string;
+        }
     }
-}
-echo ' </p>';
+    echo ' </p>';
 }
 
 //task2
 
-function task2($operator) {
-$args = func_get_args();
-$args2 = $args;
-unset($args2[0]);
+function task2($operator)
+{
+    $args = func_get_args();
+    $args2 = $args;
+    unset($args2[0]);
 
-$res = 0;
+    $res = 0;
 
-switch($operator){
-    case '+':
+    switch ($operator) {
+        case '+':
             foreach ($args2 as $arg) {
-            $res += $arg;
+                $res += $arg;
             }
-    break;
+            break;
 
-    case '-':
+        case '-':
             foreach ($args2 as $arg) {
                 $res -= $arg;
             }
-    break;
+            break;
 
-    case '*':
+        case '*':
             foreach ($args2 as $arg) {
                 $res *= $arg;
             }
-    break;
+            break;
 
-    case '/':
+        case '/':
             foreach ($args2 as $arg) {
                 $res /= $arg;
             }
-    break;
+            break;
 
-    default:
-    echo 'The operator is not valid';
+        default:
+            echo 'The operator is not valid';
 
 
-}
+    }
 
-echo $res . '<br>';
+    echo $res . '<br>';
 
 }
 
 //task3
 
-function task3($int1, $int2){
+function task3($row, $col)
+{
     echo '<table>';
-    if($int1 !==0 && $int2 !== 0){
-    for($i=1;$i<=$int1;$i++){
+    if ($row !== 0 && $col !== 0) {
+        for ($i = 1; $i <= $row; $i++) {
             echo '<tr>';
-            $y = 1;
-            for($x=1;$x<=$int2;$x++){
+            for ($x = 1; $x <= $col; $x++) {
                 echo '<td>';
-                $res = $i * $y;
-                $y++;
-                echo $res .'</td>';
+                $res = $i * $x;
+                echo $res . '</td>';
             }
             echo '</tr>';
-            }
+        }
     }
-echo '</table>';
+    echo '</table>';
 }
 
 // task4
 
-function task4() {
+function task4()
+{
     echo date('d.m.Y H:i') . '<br>';
     $time = time();
     echo date('d.m.Y H:i:s', $time) . '<br>';
@@ -89,7 +91,8 @@ function task4() {
 
 //task5 
 
-function task5() {
+function task5()
+{
     $str = 'Карл у Клары украл Кораллы';
     $str2 = 'Две бутылки лимонада';
     echo str_replace('К', '', $str);
@@ -101,17 +104,15 @@ function task5() {
 //task6 
 
 
-function task6($file) {
+function task6($file)
+{
 
     if (file_exists($file) && is_readable($file)) {
-        $thisfile = fopen($file, 'w');
-        fwrite($thisfile, 'Hello again!');
-        $thisfile = fopen($file, 'r'); 
-        $contents = fread($thisfile, filesize($file));
+        file_put_contents($file, 'Hello again!');
+        $contents = file_get_contents($file);
         echo $contents;
-        fclose($thisfile);
-    }
-    else {
+
+    } else {
         echo 'File is not exists';
     }
 }
