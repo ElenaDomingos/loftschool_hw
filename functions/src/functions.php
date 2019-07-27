6df064db355b25104c8351d92c3c7e97fb3c9e1c
Elena Domingos <elena-lezhneva@mail.ru>
2019-07-27 16:32:38 -0300

    Add files via upload

--- functions/src/functions.php
+++ functions/src/functions.php
@@ -1,119 +1,127 @@
 <?php
 
-
 //task 1
 
-function task1(array $strings, bool $implode = false)
+function task1($xml_file)
 {
-    echo '<p>';
-    foreach ($strings as $string) {
-        if (!$implode) {
-            echo '<p>' . $string . ' </p>';
-        } else {
-            echo $string;
+    $fileData = file_get_contents($xml_file);
+    $xml = new SimpleXMLElement($fileData);
+    echo '<div class="container">';
+
+    foreach ($xml->Address as $address) {
+        echo '<div class="col-6">';
+        echo 'Name: ' . $address->Name . '</br>';
+        echo 'Addrees: ' . $address->Street . '</br>';
+        echo 'City: ' . $address->City . '</br>';
+        echo 'State: ' . $address->State . '</br>';
+        echo 'ZIP: ' . $address->Zip . '</br>';
+        echo 'Country: ' . $address->Country . '</br>';
+
+
+        foreach ($xml->DeliveryNotes as $notes) {
+            echo 'Notes: ' . $notes . ' <br><br>';
+        }
+
+        echo '</div>';
+    }
+
+
+    foreach ($xml->Items->Item as $item) {
+        echo '<div class="col-6">';
+        echo 'Product Name: ' . $item->ProductName . '<br>';
+        echo 'Quantity: ' . $item->Quantity . '<br>';
+        echo 'Price: ' . $item->USPrice . '<br>';
+        echo 'Comment: ' . $item->Comment . '<br><br>';
+        echo '</div>';
+
+    }
+
+    echo '</div>';
+
+
+}
+
+// task 2
+
+function task2()
+{
+    $library = [
+        'books' => [
+            ['name' => 'The Dead Souls', 'author' => 'Gogol', 'pages' => 230]
+        ]
+
+
+    ];
+
+    $json_data = json_encode($library);
+    file_put_contents('output.json', $json_data);
+
+    $arrayOpen = file_get_contents('output.json');
+    $rand =  rand(0,1);
+
+    if($rand){
+        $arrayOpen['books']['author']= 'Oblomov';
+        echo 'Массив был изменен';
+    }
+    $json_data2 = json_decode($arrayOpen);
+
+    file_put_contents('output2.json', $json_data2);
+
+    $openOutput = file_get_contents('output.json');
+    $openOutput2 = file_get_contents('output2.json');
+
+    $decodOutput = json_decode($openOutput, true);
+    $decodOutput2 = json_decode($openOutput2, true);
+
+    print_r($decodOutput);
+    echo '<br>';
+    print_r($decodOutput2);
+
+    $diff = array_diff_assoc($decodOutput, $decodOutput2);
+
+    if(!empty($diff)){
+        echo 'Разница в массивах: ';
+        print_r($diff);
+    }
+
+}
+
+// task 3
+
+function task3()
+{
+    $array = [];
+    for ($i = 0; $i <= 50; $i++) {
+        $number = rand(1, 100);
+        $array[$i] = $number;
+
+    }
+
+
+    $randomNumbers = fopen('randomnumbers.csv', 'w');
+    fputcsv($randomNumbers, $array, ',');
+    $even = fopen('randomnumbers.csv', 'r');
+    while ($str = fgetcsv($even, 1000 * 1024, ',')) {
+        foreach ($str as $item) {
+            if ($item % 2 === 0) {
+                echo $item . ' ';
+            }
+
         }
     }
-    echo ' </p>';
-}
-
-//task2
-
-function task2($operator)
-{
-    $args = func_get_args();
-    $args2 = $args;
-    unset($args2[0]);
-
-    $res = 0;
-
-    switch ($operator) {
-        case '+':
-            foreach ($args2 as $arg) {
-                $res += $arg;
-            }
-            break;
-
-        case '-':
-            foreach ($args2 as $arg) {
-                $res -= $arg;
-            }
-            break;
-
-        case '*':
-            foreach ($args2 as $arg) {
-                $res *= $arg;
-            }
-            break;
-
-        case '/':
-            foreach ($args2 as $arg) {
-                $res /= $arg;
-            }
-            break;
-
-        default:
-            echo 'The operator is not valid';
-
-
-    }
-
-    echo $res . '<br>';
-
-}
-
-//task3
-
-function task3($row, $col)
-{
-    echo '<table>';
-    if ($row !== 0 && $col !== 0) {
-        for ($i = 1; $i <= $row; $i++) {
-            echo '<tr>';
-            for ($x = 1; $x <= $col; $x++) {
-                echo '<td>';
-                $res = $i * $x;
-                echo $res . '</td>';
-            }
-            echo '</tr>';
-        }
-    }
-    echo '</table>';
-}
-
-// task4
-
-function task4()
-{
-    echo date('d.m.Y H:i') . '<br>';
-    $time = time();
-    echo date('d.m.Y H:i:s', $time) . '<br>';
-}
-
-//task5 
-
-function task5()
-{
-    $str = 'Карл у Клары украл Кораллы';
-    $str2 = 'Две бутылки лимонада';
-    echo str_replace('К', '', $str);
-    echo '<br>';
-    echo str_replace('Две', 'Три', $str2);
     echo '<br>';
 }
 
-//task6 
+// task 4
+
+function task4() {
+$fp = fopen('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json' , 'r');
+file_put_contents('filejson.json', $fp);
+$file = file_get_contents('filejson.json');
+$obj = json_decode($file, true);
+echo 'Page id is: '. $obj['query']['pages']['15580374']['pageid'] . '<br>';
+echo 'Page title is : '. $obj['query']['pages']['15580374']['title'];
 
 
-function task6($file)
-{
 
-    if (file_exists($file) && is_readable($file)) {
-        file_put_contents($file, 'Hello again!');
-        $contents = file_get_contents($file);
-        echo $contents;
-
-    } else {
-        echo 'File is not exists';
-    }
 }
-   
